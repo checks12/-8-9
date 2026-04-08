ba01fad580916fcde539cfff82242b348480fe2c
checks12 <grigorijorlov53@gmail.com>
2026-04-08 08:23:18 +0300

    Add files via upload

---
+++ Построить таблицу значений функции у = f(x) для х є [а, b].cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b, h, x, y;
+
+    cout << "Введите a: ";
+    cin >> a;
+
+    cout << "Введите b: ";
+    cin >> b;
+
+    cout << "Введите шаг h: ";
+    cin >> h;
+
+    cout << "x\t y" << endl;
+
+    for (x = a; x <= b; x += h)
+    {
+        // пример функции
+        if (x == 0)   // точка где функция не определена
+        {
+            cout << x << "\t функция не определена" << endl;
+        }
+        else
+        {
+            y = 1 / x;   // сюда ставится нужная функция
+            cout << x << "\t" << y << endl;
+        }
+    }
+
+    return 0;
+}
